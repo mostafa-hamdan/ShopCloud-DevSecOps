8f0e491cd1efcc952d049b290f4e17bea9eef607
Mostafa Hamdan <mostafa.hadman03@hotmail.com>
2026-05-01 12:07:22 +0300

    chore: prepare stage 4 data services

--- infra/terraform/envs/dev/outputs.tf
+++ infra/terraform/envs/dev/outputs.tf
@@ -9,3 +9,16 @@ output "cluster_name" {
 output "invoice_bucket" {
   value = module.s3.bucket_name
 }
+
+output "rds_endpoint" {
+  value = module.rds.endpoint
+}
+
+output "rds_master_user_secret_arn" {
+  value     = module.rds.master_user_secret_arn
+  sensitive = true
+}
+
+output "redis_primary_endpoint" {
+  value = module.redis.primary_endpoint
+}
--- infra/terraform/modules/rds/outputs.tf
+++ infra/terraform/modules/rds/outputs.tf
@@ -1,3 +1,7 @@
 output "endpoint" {
   value = try(aws_db_instance.this[0].address, null)
 }
+
+output "master_user_secret_arn" {
+  value = try(aws_db_instance.this[0].master_user_secret[0].secret_arn, null)
+}
